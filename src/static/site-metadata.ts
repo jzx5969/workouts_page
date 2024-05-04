bc6a8dc6c5be9d0c6767e7797f850bddfa8b184d
jzx5969 <167516089+jzx5969@users.noreply.github.com>
2024-05-04 15:02:21 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -11,8 +11,7 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: '江志鑫
-    运动记录&活动热图',
+  siteTitle: '江志鑫的运动记录&活动热图',
   siteUrl: 'https://www.strava.com/athletes/111427482',
   logo: 'https://dgalywyr863hv.cloudfront.net/pictures/athletes/111427482/26085264/3/large.jpg',
   description: 'Personal site and blog',
