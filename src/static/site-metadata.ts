95958e62ac7f39a7762b64c5ed690d946d8dce1b
jzx5969 <167516089+jzx5969@users.noreply.github.com>
2024-05-04 18:57:57 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -11,7 +11,7 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: '江志鑫的运动记录&活动热图',
+  siteTitle: '江志鑫的运动记录',
   siteUrl: 'https://www.strava.com/athletes/111427482',
   logo: 'https://dgalywyr863hv.cloudfront.net/pictures/athletes/111427482/26085264/3/large.jpg',
   description: 'Personal site and blog',
@@ -22,8 +22,8 @@ const data: ISiteMetadataResult = {
       url: 'https://www.strava.com/athletes/111427482',
     },
     {
-      name: 'ICU',
-      url: 'https://intervals.icu/athlete/i94974/activities',
+      name: 'MikeMa',
+      url: 'https://mikema.vercel.app/',
     },
   ],
 };
